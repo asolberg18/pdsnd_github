fdc468f10b67962df8a17b3f7e31588259f97003
Amber Solberg <ambersolberg@gmail.com>
2020-04-29 13:18:05 -0700

    refactor: call display_stats function in main()

--- bikeshare-project.py
+++ bikeshare-project.py
@@ -228,6 +228,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
+        display_stats(df, 0)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
